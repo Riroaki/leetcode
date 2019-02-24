04f6a60cf7741d6258f6324be239888327a1c18f
Riroaki <2569126376@qq.com>
2019-02-24 12:18:53 +0800

    .

--- old/python/A+B/naive.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums, target):
-        length = len(nums)
-        for i in range(length):
-            for j in range(i + 1, length):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
