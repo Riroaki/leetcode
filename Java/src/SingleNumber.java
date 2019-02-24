deaf47300c97475f8831e414b861990dd7cfef73
Riroaki <2569126376@qq.com>
2019-02-24 12:17:49 +0800

    .

--- Java/src/SingleNumber.java
+++
@@ -1,8 +0,0 @@
-public class SingleNumber {
-    public int singleNumber(int[] nums) {
-        if (nums == null || nums.length == 0) return -1;
-        int res = nums[0];
-        for (int i = 1; i < nums.length; i++) res ^= nums[i];
-        return res;
-    }
-}
